500ef6e3134d1756694d107fc7200c7f97253d65
Adam Laycock <adam@arcath.net>
2019-12-12 11:58:29 +0000

    day 10 puzzle 1

---
+++ utils/positions.ts
@@ -0,0 +1,19 @@
+export class Point{
+  constructor(public x: number, public y: number){}
+
+  distance(from: Point){
+    return (Math.abs(this.x - from.x) + Math.abs(this.y - from.y))
+  }
+
+  equal(to: Point){
+    return (this.x === to.x && this.y === to.y)
+  }
+
+  angleTo(to: Point){
+    const dX = to.x - this.x
+    const dY = this.y - to.y
+    const tR = Math.atan2(dY, dX)
+
+    return (tR * (180/Math.PI))
+  }
+}
